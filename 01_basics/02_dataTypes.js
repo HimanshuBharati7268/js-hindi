a0d4e62a91e6306a757ff1ae1a7dfd27c94703cd
Himanshu Bharati <109125301+HimanshuBharati7268@users.noreply.github.com>
2023-12-18 11:35:00 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // use all js code in newer version
+
+// alert(3+3) // not run in this because we are using nodejs, it will run in browser
+
+let name = "Himanshu"
+let age = 21
+let isLoggedIn = false
+
+
+// number => range -> 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "himanshu")
+console.log(typeof age)
+
+console.log(typeof undefined) // => undefined
+console.log(typeof null) // =>object
+
