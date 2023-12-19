e0aadf20e1e1c354d57aceabb29dfdde39fb152a
Himanshu Bharati <109125301+HimanshuBharati7268@users.noreply.github.com>
2023-12-19 04:42:38 +0000

    datatype conversion

---
+++ 01_basics/03_conversionofoperations.js
@@ -0,0 +1,60 @@
+// let score = "33";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // conversion string to integer
+// let num = Number(score);
+// console.log(typeof(num));
+
+// let score = "33abs";
+// let num = Number(score);
+// console.log(typeof(num));
+// console.log(num); // give o/p => NaN -> not a number
+
+// let score = null;
+// let num = Number(score);
+// console.log(typeof(num));
+// console.log(num); // give o/p => NaN -> not a number
+
+// let score = undefined;
+// let num = Number(score);
+// console.log(typeof(num));
+// console.log(num); // give o/p => NaN -> not a number
+
+// let score = true;
+// let num = Number(score);
+// console.log(typeof(num));
+// console.log(num); // give o/p => NaN -> not a number
+
+// let score = "";
+// let num = Number(score);
+// console.log(typeof(num));
+// console.log(num); // give o/p => NaN -> not a number
+
+// let score = "Himanshu";
+// let num = Number(score);
+// console.log(typeof(num));
+// console.log(num); // give o/p => NaN -> not a number
+
+
+// let score = 1;
+// let num = Boolean(score);
+// console.log(typeof(num));
+// console.log(num); // give o/p => NaN -> not a number
+
+
+// let score = "";
+// let num = Boolean(score);
+// console.log(typeof(num));
+// console.log(num); // give o/p => NaN -> not a number
+
+
+// let score = "himanshu";
+// let num = Boolean(score);
+// console.log(typeof(num));
+// console.log(num); // give o/p => NaN -> not a number
+
+let num = 33;
+let s = String(num);
+console.log(typeof(s));
+console.log(s);
