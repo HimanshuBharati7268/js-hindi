31723b5dc016f8b50ac7fd7813b50b504f6daf51
Himanshu Bharati <109125301+HimanshuBharati7268@users.noreply.github.com>
2023-12-19 17:55:33 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,37 @@
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2>=3);
+// console.log(2<=3);
+// console.log(2==3);
+// console.log(2!=3);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log(1 < "2");
+// console.log(1 < "02");
+
+
+// console.log(null > 1)
+// console.log(null < 1)
+// console.log(null >= 1)
+// console.log(null <= 1)
+// console.log(null == 1)
+
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+// console.log(null == 0)
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+// console.log(undefined == 0)
+
+
+console.log("2" == 2) // but
+// strict check => value and data types both checks
+console.log("2" === 2) 
